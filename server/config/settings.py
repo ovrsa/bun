483436524200617d1fd7f4339c3cdf334d94ef18
ovrsa <sufferin.in@gmail.com>
2024-11-01 09:51:28 +0900

    feat(auth): extend access token expiration time

--- server/config/settings.py
+++ server/config/settings.py
@@ -87,7 +87,7 @@ REST_FRAMEWORK = {
 
 # TODO: 本番環境では変更する
 SIMPLE_JWT = {
-    'ACCESS_TOKEN_LIFETIME': timedelta(minutes=5),
+    'ACCESS_TOKEN_LIFETIME': timedelta(minutes=60),
     'REFRESH_TOKEN_LIFETIME': timedelta(days=1),
     'ROTATE_REFRESH_TOKENS': True,
     'BLACKLIST_AFTER_ROTATION': True,
