b6486e718504b7493671840b0710bcc1caf0ba6f
ovrsa <sufferin.in@gmail.com>
2024-11-22 08:57:57 +0900

    chore(deps): update msw version

--- client/mocks/handlers.ts
+++ client/mocks/handlers.ts
@@ -1,4 +1,4 @@
-import { rest } from 'msw';
+import { http } from 'msw';
 
 /**
  * ログインAPIのモック
@@ -7,7 +7,7 @@ import { rest } from 'msw';
  */
 export const handlers = [
   // ログインAPIのPOSTリクエストをモック
-  rest.post('/api/auth/login/', async (req, res, ctx) => {
+  http.post('/api/auth/login/', async (req, res, ctx) => {
     const { email, password } = await req.json();
     // 正しい認証情報が提供された場合
     if (email === 'test@example.com' && password === 'password') {
@@ -18,7 +18,7 @@ export const handlers = [
   }),
 
   // ユーザー情報取得APIのGETリクエストをモック
-  rest.get('/api/user', (req, res, ctx) => {
+  http.get('/api/user', (req, res, ctx) => {
     return res(ctx.status(200), ctx.json({ id: 1, name: 'John Doe' })); // 成功レスポンスを返す
   }),
 ];
