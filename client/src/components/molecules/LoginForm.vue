302a0a93a4ae27dd1e5e419801b2f9b302cb7266
ovrsa <sufferin.in@gmail.com>
2024-11-24 23:29:06 +0900

    fix(auth): update login func

--- client/src/components/molecules/LoginForm.vue
+++ client/src/components/molecules/LoginForm.vue
@@ -3,10 +3,10 @@
     <h2 class="text-2xl mb-20 text-center">Login</h2>
 
     <InputField
-      type="email"
-      v-model="email"
-      placeholder="Email"
-      :errorMessage="errors.email ? 'Email is required.' : ''"
+      type="text"
+      v-model="username"
+      placeholder="Username"
+      :errorMessage="errors.username ? 'Username is required.' : ''"
     />
 
     <InputField
@@ -15,12 +15,6 @@
       placeholder="Password"
       :errorMessage="errors.password ? 'Password is required.' : ''"
     />
-
-    <!-- <div class="text-center pt-5">
-      <a href="" class="text-blue-600 text-sm hover:underline">
-        Forgot your password?
-      </a>
-    </div> -->
 
     <Button type="submit">Log In</Button>
 
@@ -38,48 +32,39 @@
 </template>
 
 <script setup lang="ts">
-import { ref } from 'vue'
-import { useStore } from 'vuex'
-import { useRouter } from 'vue-router'
-import type { AxiosError } from 'axios'
-import InputField from '@/components/atoms/InputField.vue'
-import Button from '@/components/atoms/Button.vue'
-import apiClient from '@/services/auth'
+import { ref } from "vue";
+import { useStore } from "vuex";
+import { useRouter } from "vue-router";
+import InputField from "@/components/atoms/InputField.vue";
+import Button from "@/components/atoms/Button.vue";
+import apiClient from "@/services/auth";
 
-const email = ref('')
-const password = ref('')
-const errors = ref({ email: false, password: false })
-const errorMessage = ref('')
+const username = ref("");
+const password = ref("");
 
-const store = useStore()
-const router = useRouter()
+const errors = ref({ username: false, password: false });
+const errorMessage = ref("");
+
+const store = useStore();
+const router = useRouter();
 
 const onSubmit = async () => {
-  errors.value.email = !email.value
-  errors.value.password = !password.value
+  errors.value.username = !username.value;
+  errors.value.password = !password.value;
 
-  if (!errors.value.email && !errors.value.password) {
+  if (!errors.value.username && !errors.value.password) {
     try {
-      const response = await apiClient.post('login/', {
-        email: email.value,
+      const response = await apiClient.post("login/", {
+        username: username.value,
         password: password.value,
-      })
-      await store.dispatch('auth/checkAuth')
-      router.push('/')
-    } catch (err: unknown) {
-      const error = err as AxiosError
+      });
 
-      if (error.response && error.response.data) {
-        errorMessage.value = Object.values(
-          error.response.data as Record<string, any>
-        )
-          .reduce((acc, val) => acc.concat(val), [])
-          .join(' ')
-      } else {
-        errorMessage.value = 'ログインに失敗しました。'
-        console.error(`ログインに失敗しました。${error}`)
-      }
+      await store.dispatch("auth/checkAuth");
+      router.push("/");
+    } catch (err: unknown) {
+      errorMessage.value = "ログインに失敗しました。";
+      console.error(`Login failed: ${err}`);
     }
   }
-}
+};
 </script>
